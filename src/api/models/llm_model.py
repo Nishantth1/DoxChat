f9e9e67766e0be785a20593cb1ddf8934b8e6a1e
Nishantth1 <97899868+Nishantth1@users.noreply.github.com>
2024-06-08 20:04:29 +0530

    Update llm_model.py

--- src/api/models/llm_model.py
+++ src/api/models/llm_model.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 
 # Assuming you have obtained your API key (replace with your actual key)
-API_KEY = "AIzaSyCrgUC2JXeqQ2CnhsI2x8c_N6OvQoKmu1c"
+API_KEY = "API_KEY"
 
 def call_llm(prompt, max_length=1000):
     genai.configure(api_key=API_KEY)
